af4d70397f47fe35ab21877345f16f396b100bf3
hayen <hathiyenlg@gmail.com>
2022-08-23 10:27:22 +0700

    init project

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Maxiromanoff on 20/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
